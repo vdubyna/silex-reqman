07bb250133131ca94508febd8dc0580dd7ac3307
Volodymyr Dubyna <vdubyna@speroteck.com>
2013-07-20 12:47:28 +0300

    Refactor js code

--- karma.conf.js
+++ karma.conf.js
@@ -47,7 +47,7 @@ autoWatch = true;
 // - Safari (only Mac)
 // - PhantomJS
 // - IE (only Windows)
-browsers = ['Chrome'];
+browsers = ['Firefox'];
 
 // If browser does not capture in given timeout [ms], kill it
 captureTimeout = 60000;
